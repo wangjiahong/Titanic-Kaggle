d660a3b15779838d543c9a630870b0c2a35986eb
wangjiahong666 <wangjiahong666@gmail.com>
2016-08-23 10:11:00 +0300

    add os.chdir( change diricitory) to make it easier

--- xgboost_toy_example.py
+++ xgboost_toy_example.py
@@ -6,16 +6,21 @@ Created on Thu Aug 11 23:15:22 2016
 """
 
 import xgboost as xgb
+
+import os
+os.chdir("D:/git_repository/xgboost/") 
+pwd
 # read in data
-dtrain = xgb.DMatrix('D:/git_repository/xgboost/demo/data/agaricus.txt.train')
-dtest = xgb.DMatrix('D:/git_repository/xgboost/demo/data/agaricus.txt.test')
+dtrain = xgb.DMatrix('demo/data/agaricus.txt.train')
+dtest = xgb.DMatrix('demo/data/agaricus.txt.test')
 # specify parameters via map
 param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
 num_round = 200
 bst = xgb.train(param, dtrain, num_round)
+
 # make prediction
 preds = bst.predict(dtest)
-preds
+print preds
 
 
 
@@ -36,4 +41,8 @@ bst = xgb.train(param, dtrain, num_round)
 # make prediction
 # ntree_limit must not be 0
 preds = bst.predict(dtest, ntree_limit=num_round)
-preds
+print preds
+
+
+
+
