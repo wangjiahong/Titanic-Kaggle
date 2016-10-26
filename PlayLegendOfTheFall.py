a0c0814820bd0e36c94bb86d7c10adb84405670f
wangjiahong666 <wangjiahong666@gmail.com>
2016-10-26 23:14:17 +0300

    #Add new function#: could play random music

--- PlayLegendOfTheFall.py
+++ PlayLegendOfTheFall.py
@@ -4,17 +4,49 @@ Created on Wed Oct 26 22:16:05 2016
 
 @author: Jiahong
 """
-def playMusic():
-    import pygame
-    from pygame import mixer
-    mixer.init()
-    mixer.music.load("E:/Legends Of The Fall.mp3")
-    mixer.music.play()
-#mixer.music.stop()
+import pygame
+from pygame import mixer
+import glob
+from random import randint
 
+
+musics = glob.glob("J:/MY MUSIC/nice mood/*.mp3")
+
+
+
+n_musics = len(musics)
+
+
+
+def playMusic():
+    # Generate random number:
+    random_number = randint(0, len(musics)- 1)
+
+
+        # Get music name
+    music_name = musics[random_number]
+
+    # Load music
+    mixer.init()
+    mixer.music.load(music_name)
+
+    mixer.music.play()
 
 
 def stopMusic():
     mixer.music.stop()
+    
+    
+playMusic()
 stopMusic()
+
+
+
+
+
+
+
+
+
+
 
