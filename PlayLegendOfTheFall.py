f142e8b98121f33c31e0ac5a04748f86e0ddea9e
Jiahong Wang <wangjiahong666@gmail.com>
2016-10-29 11:53:20 +0300

    solve 1 commit behind problem

--- PlayLegendOfTheFall.py
+++ PlayLegendOfTheFall.py
@@ -10,14 +10,14 @@ musics = glob.glob("J:/MY MUSIC/nice mood/*.mp3")
 n_musics = len(musics)
 
 
-test
+
 
 def playMusic():
     # Generate random number:
     random_number = randint(0, len(musics)- 1)
 
 
-        # Get music name
+    # Get music name
     music_name = musics[random_number]
 
     # Load music
